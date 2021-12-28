f93a3a7fbbba08fe7dfa2b6d1fa99fbb7871276f
uas-wildan205410134 <wildanassyidiq142@gmail.com>
2021-12-28 09:25:46 +0700

    initial commit

--- index.php
+++ index.php
@@ -1,8 +1,1 @@
-<?php
-include "koneksi.php";
-session_start();
-if (!isset($_SESSION['username'])){
-header ("location:login.php");
-}
-?>
-Berhasil Login, <a href="logout.php">Logout</a>
+<?php include_once("index.html"); ?>
